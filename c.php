df43ae29fa47d6ecf96e60d3d01e5b492c97b2bb
Backedguci <83763360+Backedguci@users.noreply.github.com>
2021-06-12 20:40:47 +0300

    Update c.php

--- c.php
+++ c.php
@@ -1,1 +1,9 @@
 
+
+echo "Salut, sunt un script PHP!";
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
