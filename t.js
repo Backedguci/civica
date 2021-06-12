cafef0a84584efbf2f74fb75be2508f6f35e108b
Backedguci <83763360+Backedguci@users.noreply.github.com>
2021-06-12 21:11:00 +0300

    Update t.js

--- t.js
+++ t.js
@@ -1,4 +1,2 @@
-$("#save-btn").click(function() {
-  var blob = new Blob(["test text"], {type: "text/plain;charset=utf-8"});
-  saveAs(blob, "testfile1.txt");
-});
+var fs=require('fs')
+fs.readFileSync('c.txt')
