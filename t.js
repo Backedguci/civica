4622537570c56babe4c6b3f8dce5d6cb5444e199
Backedguci <83763360+Backedguci@users.noreply.github.com>
2021-06-13 12:01:30 +0300

    Update t.js

--- t.js
+++ t.js
@@ -1,2 +1,31 @@
-var fs= require('fs')
-fs.readFileSync('c.txt')
+const pass = "32MPar"
+var tried =window.prompt("pune parola")
+if (pass == tried){
+alert("parola corecta")
+  
+  
+var  nu =window.prompt("pune numele celui care vrei sa il votezi");
+var answer = confirm("iesti sigur?")
+if (answer == false){
+alert("refresh the page")
+                   }
+else                               {
+if (nu == "Teodora"){
+
+document.getElementById("p1").innerHTML = "ai votat cu " + nu;
+                    }
+
+else if(nu=="Elena") {
+
+document.getElementById("p1").innerHTML = "ai votat cu " + nu;
+
+
+}
+else{
+alert("nume invalid incearca din nou")
+}
+                                   }
+                  }
+else {
+alert("parola incorecta")
+}
