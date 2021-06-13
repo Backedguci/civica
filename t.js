97683309ba85f33f6603025477704f603db0444a
Backedguci <83763360+Backedguci@users.noreply.github.com>
2021-06-13 12:25:29 +0300

    Update t.js

--- t.js
+++ t.js
@@ -2,30 +2,19 @@ const pass = "32MPar"
 var tried =window.prompt("pune parola")
 if (pass == tried){
 alert("parola corecta")
-  
-  
-var  nu =window.prompt("pune numele celui care vrei sa il votezi");
-var answer = confirm("iesti sigur?")
-if (answer == false){
-alert("refresh the page")
-                   }
-else                               {
-if (nu == "Teodora"){
-
-document.getElementById("p1").innerHTML = "ai votat cu " + nu;
-                    }
-
-else if(nu=="Elena") {
-
-document.getElementById("p1").innerHTML = "ai votat cu " + nu;
-
-
+  function getVote(int) {
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("poll").innerHTML=this.responseText;
+    }
+                      }
+  xmlhttp.open("GET","poll_vote.php?vote="+int,true);
+  xmlhttp.send();
 }
-else{
-alert("nume invalid incearca din nou")
-}
-                                   }
-                  }
+
+  
+
 else {
 alert("parola incorecta")
 }
